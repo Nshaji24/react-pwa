26fded510356a5b39997e3e7160a7f662a0315a9
Noel Shaji <66273445+Nshaji24@users.noreply.github.com>
2021-08-04 23:27:20 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+workbox.precaching.precacheAndRoute([{"revision":"2ec802ecbfe57c127a9b50eeda309adc","url":"index.html"},
+{"revision":"33dbdd0177549353eeeb785d02c294af","url":"logo192.png"},
+{"revision":"917515db74ea8d1aee6a246cfbcc0b45","url":"logo512.png"},
+{"revision":"0d278609d8e024a479be8c9fe7614d85","url":"static/css/main.8c8b27cf.chunk.css"},
+{"revision":"3d8cdf48b3af0257ce08d89bdef6b16e","url":"static/js/2.d0eb047a.chunk.js"},
+{"revision":"ed9558a1dbb8e5b2aed67a5210750409","url":"static/js/main.1aba7058.chunk.js"},
+{"revision":"519132ea16023b819c3bfe5f087fe6f1","url":"static/js/runtime-main.406dd86f.js"}]);
